1904fa7cf948b416738ce94dbe0307bf9433b3b5
vonsyus <n.volosevichh@gmail.com>
2020-04-08 20:21:19 +0300

    add few blocks

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,33 @@
 $(document).ready(function() {
-    let owl = $('.owl-carousel');
-    owl.owlCarousel({
+    let owl1 = $('#owl1');
+    owl1.owlCarousel({
         items: 1,
         loop: false,
         margin: 10,
         autoWidth: true
     });
 
-    $('#left').click(function() {
-        owl.trigger('prev.owl.carousel');
+    $('#left1').click(function() {
+        owl1.trigger('prev.owl.carousel');
     });
 
-    $('#right').click(function() {
-        owl.trigger('next.owl.carousel');
+    $('#right1').click(function() {
+        owl1.trigger('next.owl.carousel');
+    });
+
+    let owl2 = $('#owl2');
+    owl2.owlCarousel({
+        items: 3,
+        loop: false,
+        margin: 22,
+        slideBy: 3
+    });
+
+    $('#left2').click(function() {
+        owl2.trigger('prev.owl.carousel');
+    });
+
+    $('#right2').click(function() {
+        owl2.trigger('next.owl.carousel');
     });
 });
